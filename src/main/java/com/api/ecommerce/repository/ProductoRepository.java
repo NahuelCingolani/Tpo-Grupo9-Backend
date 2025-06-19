bf427ca0629c6794a8d8039b60d628e7f5116d6b
NahuelCingolani <ncingolani@uade.edu.ar>
2025-06-19 15:50:54 -0300

    Se arreglan problemas esctructurales

--- src/main/java/com/api/ecommerce/repository/ProductoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.api.ecommerce.repository;
-
-import com.api.ecommerce.model.Producto;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import java.util.Optional;
-
-public interface ProductoRepository extends MongoRepository<Producto, String> {
-    Optional<Producto> findByNombreAndEquipoAndJugador(String nombre, String equipo, String jugador);
-}
