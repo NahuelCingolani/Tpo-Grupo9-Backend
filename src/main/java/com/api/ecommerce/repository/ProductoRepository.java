b2e652448bbba079d7ebd13f3b059251dbf49269
joanavillca <joanavillca@gmail.com>
2025-06-24 17:46:35 -0300

    se agrego filtro de categoria

--- src/main/java/com/api/ecommerce/repository/ProductoRepository.java
+++ src/main/java/com/api/ecommerce/repository/ProductoRepository.java
@@ -2,8 +2,13 @@ package com.api.ecommerce.repository;
 
 import com.api.ecommerce.model.Producto;
 import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
 import java.util.Optional;
 
 public interface ProductoRepository extends MongoRepository<Producto, String> {
     Optional<Producto> findByNombreAndEquipoAndJugador(String nombre, String equipo, String jugador);
+
+    List<Producto> findByEquipoIgnoreCase(String equipo);
 }
+
